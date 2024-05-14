3fa94dd850fff1cf571f74931dea4ad4146a76a5
Abhijith David <abhijith.david@ucalgary.ca>
2024-05-13 22:29:24 -0600

    Add files via upload
    added & changed README.md

---
+++ assignment1code.py
@@ -0,0 +1,12 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Student Name: Abhijith David
+
+studentName = "Abhijith David"
+skill = "intense knowledge about cars"
+quote = " It's not about the destination, it's about the ride."
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+print("My best skill is " + skill + " and I own a '19 Mustang.")
+print("And the quote goes like..." + quote)
